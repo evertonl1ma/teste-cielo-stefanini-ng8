77c822e77caff2c8dd2949eeebd1e517d7acfa85
Everton Lima <ever-tonlima@hotmail.com>
2019-09-09 01:23:15 -0300

    Constants

--- services/api.service.ts
+++ services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError, tap, delay } from 'rxjs/operators'
+import { Constants } from '../utils/api';
 
 const httpOptions = {
   headers: new HttpHeaders({ "Content-Type": "application/json", "Authorization": "c31z" })
@@ -14,8 +15,8 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getDataFunc() {
-  	return this.http.get('http://localhost:3000/lancamentos',  httpOptions).pipe(
-  							//delay(3000),
+  	return this.http.get(`${Constants.SERVICE_URL}/lancamentos`,  httpOptions).pipe(
+  							delay(3000),
   							tap(response => console.log(' response', response)),
   							map(response => response)
   							)
---
+++ utils/api.ts
@@ -0,0 +1,5 @@
+export class Constants {
+
+  public static get SERVICE_URL(): string { return "http://localhost:3000"; };
+
+}
